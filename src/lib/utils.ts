de6cba2ffe49b5dc2f82b13e961df1ba2112702c
Tomi Chen <tctree333@users.noreply.github.com>
2023-06-27 20:20:01 -0700

    add option to not throw on checks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,33 +2,49 @@ import { error } from '@sveltejs/kit';
 import { getUserInfo } from './db';
 import { TournamentRoles } from '@prisma/client';
 
-export async function checkIsDirector(userId: string, tournamentId: string) {
+export async function checkIsDirector(userId: string, tournamentId: string, throwError = true) {
 	const user = await getUserInfo(userId);
 
 	if (user === false) {
-		throw error(500, 'Error fetching user information');
+		if (throwError) {
+			throw error(500, 'Error fetching user information');
+		} else {
+			return false;
+		}
 	}
 	const userRole = user.roles.find(
 		(role) => role.role === TournamentRoles.DIRECTOR && role.tournament.id === tournamentId
 	);
 	if (userRole == undefined) {
-		throw error(403, 'You do not have permission to view this page');
+		if (throwError) {
+			throw error(403, 'You do not have permission to view this page');
+		} else {
+			return false;
+		}
 	}
 
 	return true;
 }
 
-export async function checkEventPerms(userId: string, eventId: bigint) {
+export async function checkEventPerms(userId: string, eventId: bigint, throwError = true) {
 	const user = await getUserInfo(userId);
 
 	if (user === false) {
-		throw error(500, 'Error fetching user information');
+		if (throwError) {
+			throw error(500, 'Error fetching user information');
+		} else {
+			return false;
+		}
 	}
 	const userRole = user.roles.find(
 		(role) => role.role === TournamentRoles.DIRECTOR || role.event?.id === eventId
 	);
 	if (userRole == undefined) {
-		throw error(403, 'You do not have permission to view this page');
+		if (throwError) {
+			throw error(403, 'You do not have permission to view this page');
+		} else {
+			return false;
+		}
 	}
 
 	return true;
