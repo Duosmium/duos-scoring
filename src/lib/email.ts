d3314e70f68f1f94cfa78cf600af79de74facab7
Tomi Chen <tctree333@users.noreply.github.com>
2023-12-04 00:53:12 -0800

    update email template

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -14,13 +14,13 @@ export async function sendInvite(
 			from: 'Duosmium Scoring <invite@scoring.duosmium.org>',
 			to: [email],
 			subject: `Join the ${tournamentName}!`,
-			// TODO: make email pretty
 			html:
-				`<p>You have been invited to join ${tournamentName} on Duosmium Scoring!</p>` +
+				`<h2>You have been invited to join ${tournamentName} on Duosmium Scoring!</h2>\n` +
 				((events?.length ?? 0) > 0
-					? `<p>You will be added to these events: ${events?.join(', ')}</p>`
+					? `<p>You will be added to these events: ${events?.join(', ')}</p>\n`
 					: '') +
-				`<p><a href="https://scoring.duosmium.org/invite/${invite}">Click here</a> to accept the invite.</p>`,
+				`<p><a href="https://scoring.duosmium.org/invite/${invite}">Click here</a> to accept the invite.</p>\n` +
+				'<hr /><p style="color:#898989;font-size:12px;">Duosmium Scoring</p>',
 			text:
 				`You have been invited to join ${tournamentName} on Duosmium Scoring!\n` +
 				((events?.length ?? 0) > 0
