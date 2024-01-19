5b4f626f134bcc79456a095eae060268b167deab
Tomi Chen <tctree333@users.noreply.github.com>
2024-01-19 12:24:37 -0800

    sort dashboard tournaments

--- src/routes/(root)/(app)/dashboard/+page.svelte
+++ src/routes/(root)/(app)/dashboard/+page.svelte
@@ -4,6 +4,13 @@
 	import { A, Button, Card, Heading, Li, List, P } from 'flowbite-svelte';
 
 	export let data: PageData;
+
+	$: data.user.roles = data.user.roles.sort(
+		(a, b) =>
+			b.tournament.startDate.valueOf() - a.tournament.startDate.valueOf() ||
+			a.tournament.name.localeCompare(b.tournament.name) ||
+			a.tournament.division.localeCompare(b.tournament.division)
+	);
 </script>
 
 <Head title="Dashboard | Duosmium Scoring" />
@@ -18,9 +25,18 @@
 	<div class="grid">
 		{#each data.user.roles as role}
 			<Card size="md">
-				<Heading tag="h3" customSize="text-md" class="mb-6"
+				<Heading tag="h3" customSize="text-md" class="mb-2"
 					>{role.tournament.year} {role.tournament.name} {role.tournament.division}</Heading
 				>
+				<P class="mb-6"
+					>{role.tournament.startDate.toUTCString() === role.tournament.endDate.toUTCString()
+						? role.tournament.startDate.toUTCString().split(' ').slice(0, 4).join(' ')
+						: role.tournament.startDate.toUTCString().split(' ').slice(0, 4).join(' ') +
+						  ' - ' +
+						  role.tournament.endDate.toUTCString().split(' ').slice(0, 4).join(' ')}
+					<br />
+					@ {role.tournament.location}
+				</P>
 				{#if role.role === 'TD'}
 					<Button href="/t/{role.tournament.id}/">Tournament Director Dashboard</Button>
 				{:else if role.role === 'SM'}
