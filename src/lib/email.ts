d8a8756c4e16a3558aeb9f90dc5300e923ae67f6
Tomi Chen <tctree333@users.noreply.github.com>
2024-01-22 21:56:22 -0800

    use smtp instead of aws sdk

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,7 +1,17 @@
+import { PRIVATE_SMTP_PASS, PRIVATE_SMTP_USER } from '$env/static/private';
 import { error } from '@sveltejs/kit';
 
-import { SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
-const ses = new SESv2Client({ region: 'us-east-1' });
+import nodemailer from 'nodemailer';
+
+const mailer = nodemailer.createTransport({
+	host: 'email-smtp.us-east-1.amazonaws.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: PRIVATE_SMTP_USER,
+		pass: PRIVATE_SMTP_PASS
+	}
+});
 
 export async function sendInvite(
 	email: string,
@@ -10,42 +20,25 @@ export async function sendInvite(
 	events?: string[]
 ) {
 	try {
-		await ses.send(
-			new SendEmailCommand({
-				FromEmailAddress: 'Duosmium Scoring <invite@scoring.duosmium.org>',
-				FeedbackForwardingEmailAddress: 'security@duosmium.org',
-				Destination: {
-					ToAddresses: [email]
-				},
-				ReplyToAddresses: ['support@duosmium.org'],
-				Content: {
-					Simple: {
-						Subject: {
-							Data: `Join the ${tournamentName}!`
-						},
-						Body: {
-							Text: {
-								Data:
-									`You have been invited to join ${tournamentName} on Duosmium Scoring!\n` +
-									((events?.length ?? 0) > 0
-										? `You will be added to these events: ${events?.join(', ')}\n`
-										: '') +
-									`Click here to accept the invite: https://scoring.duosmium.org/invite/${invite}`
-							},
-							Html: {
-								Data:
-									`<h2>You have been invited to join ${tournamentName} on Duosmium Scoring!</h2>\n` +
-									((events?.length ?? 0) > 0
-										? `<p>You will be added to these events: ${events?.join(', ')}</p>\n`
-										: '') +
-									`<p><a href="https://scoring.duosmium.org/invite/${invite}">Click here</a> to accept the invite.</p>\n` +
-									'<hr /><p style="color:#898989;font-size:12px;">Duosmium Scoring</p>'
-							}
-						}
-					}
-				}
-			})
-		);
+		await mailer.sendMail({
+			from: 'Duosmium Scoring <invite@scoring.duosmium.org>',
+			to: email,
+			replyTo: 'support@duosmium.org',
+			subject: `Join the ${tournamentName}!`,
+			text:
+				`You have been invited to join ${tournamentName} on Duosmium Scoring!\n` +
+				((events?.length ?? 0) > 0
+					? `You will be added to these events: ${events?.join(', ')}\n`
+					: '') +
+				`Click here to accept the invite: https://scoring.duosmium.org/invite/${invite}`,
+			html:
+				`<h2>You have been invited to join ${tournamentName} on Duosmium Scoring!</h2>\n` +
+				((events?.length ?? 0) > 0
+					? `<p>You will be added to these events: ${events?.join(', ')}</p>\n`
+					: '') +
+				`<p><a href="https://scoring.duosmium.org/invite/${invite}">Click here</a> to accept the invite.</p>\n` +
+				'<hr /><p style="color:#898989;font-size:12px;">Duosmium Scoring</p>'
+		});
 	} catch (err) {
 		console.error(err);
 		error(500, 'Failed to send email!');
