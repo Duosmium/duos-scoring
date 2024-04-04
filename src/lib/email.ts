da7e1eeb910a696e34cddc4d671623bf9d65a978
Jason Chang <jsc008@ucsd.edu>
2024-04-04 03:07:19 -0700

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -36,8 +36,8 @@ export async function sendInvite(
 				((events?.length ?? 0) > 0
 					? `<p>You will be added to these events: ${events?.join(', ')}</p>\n`
 					: '') +
-				`<p><a href="https://scoring.duosmium.org/invite/${invite}">Click here</a> to accept the invite. For scoring-related questions, reach out to Simon Li, the scoremaster for the tournament.</p>\n` +
-				`<p>Make sure you have access to the system before the tournament. If you are on multiple events (or the same event in both division), make sure you have access to both. If you don't, reach out in the Event Supervisor Discord Server as soon as possible.</p>\n` +
+				`<p><a href="https://scoring.duosmium.org/invite/${invite}">Click here</a> to accept the invite.</p>\n` +
+				`<p>Make sure you have access to the system before the tournament. If you are on multiple events (or the same event in both division), make sure you have access to both. If you don't, reach out to Jason (jasonchang@socalscioly.org) as soon as possible.</p>\n` +
 				'<hr /><p style="color:#898989;font-size:12px;">Duosmium Scoring</p>'
 		});
 	} catch (err) {
