4732328420d36e094bdafc829b78fbbd1bcce0da
Tomi Chen <tctree333@users.noreply.github.com>
2024-05-21 08:39:14 -0700

    try disabling instrumentation

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sentrySvelteKit({
-			autoUploadSourceMaps: false
-			// sourceMapsUploadOptions: {
-			// 	org: 'sciolyid',
-			// 	project: 'duos-scoring',
-			// 	authToken: process.env.PRIVATE_SENTRY_AUTH_TOKEN,
-			// 	telemetry: false
-			// }
+			sourceMapsUploadOptions: {
+				org: 'sciolyid',
+				project: 'duos-scoring',
+				authToken: process.env.PRIVATE_SENTRY_AUTH_TOKEN,
+				telemetry: false
+			},
+			autoInstrument: false
 		}),
 		sveltekit()
 	],
