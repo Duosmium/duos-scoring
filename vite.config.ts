1cc4311c4eb3a569555f3b460c753c3aea5a4b03
Tomi Chen <tctree333@users.noreply.github.com>
2024-05-21 08:34:00 -0700

    try disabling sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'sciolyid',
-				project: 'duos-scoring',
-				authToken: process.env.PRIVATE_SENTRY_AUTH_TOKEN,
-				telemetry: false
-			}
+			autoUploadSourceMaps: false
+			// sourceMapsUploadOptions: {
+			// 	org: 'sciolyid',
+			// 	project: 'duos-scoring',
+			// 	authToken: process.env.PRIVATE_SENTRY_AUTH_TOKEN,
+			// 	telemetry: false
+			// }
 		}),
 		sveltekit()
 	],
