0c52df12ffaa6c7c3417210479c1ffcd2f1ab906
Tomi Chen <tctree333@users.noreply.github.com>
2024-12-08 17:26:42 -0800

    try different auth redirect

--- src/routes/(root)/(app)/auth/+server.ts
+++ src/routes/(root)/(app)/auth/+server.ts
@@ -7,20 +7,25 @@ import { captureException } from '@sentry/sveltekit';
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
 	const token_hash = url.searchParams.get('token_hash');
 	const type = url.searchParams.get('type') as EmailOtpType | null;
-	const next = new URL(
+
+	const redirectTo = new URL(
 		url.searchParams.get('next') ?? '/',
 		'https://scoring.duosmium.org'
-	).pathname;
-
-	const redirectTo = new URL('https://scoring.duosmium.org');
-	redirectTo.pathname = next;
+	);
+	redirectTo.searchParams.delete('token_hash');
+	redirectTo.searchParams.delete('type');
+	redirectTo.searchParams.delete('next');
 
 	if (token_hash && type) {
-		const { error } = await supabase.auth.verifyOtp({ type, token_hash });
-		if (!error) {
+		const { error: authError } = await supabase.auth.verifyOtp({
+			type,
+			token_hash
+		});
+		if (!authError) {
 			return redirect(303, redirectTo);
 		}
-		captureException(error);
+		captureException(authError);
+		error(authError.status || 500, authError.message);
 	}
-	error(500, 'Something went wrong during authentication.');
+	error(400, 'Missing token or type');
 };
